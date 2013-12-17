52f43b8f43cb5a3402d7e6d14f6474b9b4a5a859
Henry Eklind <henry@vaert.se>
2013-12-17 13:23:38 +0100

    Added StringSize function

--- barcli/barcli.go
+++ barcli/barcli.go
@@ -1,9 +1,12 @@
 // Package barcli implements a cli frontend for progress bars.
 package barcli
 
-import "fmt"
+import (
+	"fmt"
 
-import "github.com/0xC3/progress"
+	"github.com/0xC3/progress"
+	"github.com/mewkiz/pkg/errutil"
+)
 
 // Bar represents a cli frontend of a progress.Bar object.
 type Bar struct {
@@ -19,11 +22,7 @@ func New(max int) (bar *Bar, err error) {
 	bar = new(Bar)
 	bar.backend, err = progress.New(max)
 	if err != nil {
-		return nil, err
-	}
-	err = bar.Print()
-	if err != nil {
-		return nil, err
+		return nil, errutil.Err(err)
 	}
 	return bar, nil
 }
@@ -32,64 +31,64 @@ func New(max int) (bar *Bar, err error) {
 func (bar *Bar) IncMax(add int) (err error) {
 	err = bar.backend.IncMax(add)
 	if err != nil {
-		return err
+		return errutil.Err(err)
 	}
-	return bar.Print()
+	return nil
 }
 
 // IncN increases the Cur value of bar by add and prints the bar.
 func (bar *Bar) IncN(add int) (err error) {
 	err = bar.backend.IncN(add)
 	if err != nil {
-		return err
+		return errutil.Err(err)
 	}
-	return bar.Print()
+	return nil
 }
 
 // Inc increases the Cur value of bar by one and prints the bar.
 func (bar *Bar) Inc() (err error) {
 	err = bar.backend.Inc()
 	if err != nil {
-		return err
+		return errutil.Err(err)
 	}
-	return bar.Print()
+	return nil
 }
 
 // Print prints the current progress bar.
 //
 // Note: the terminal has to be at least 14 characters in width.
-func (bar *Bar) Print() (err error) {
-	const prettyFmt = "[%s] %3d%% done"
+func (bar *Bar) StringSize(col int) (filled string, unfilled string, err error) {
+	const prettyFmt = "%s"
 
 	//    '%s' -> ''  = -2
 	//    '%%' -> '%' = -1
 	const prettySize = len(prettyFmt) - 3
-	ws := getWinSize()
-	barSize := int(ws.Col) - prettySize
+	barSize := col - prettySize
 	if barSize < 2 {
-		return fmt.Errorf("terminal too small (%d) to display progressbar.", ws.Col)
+		return "", "", errutil.NewNoPosf("terminal too small (%d) to display progressbar.", col)
 	}
 	part := bar.backend.Progress()
 	barCount := int(part * float64(barSize))
 	percent := int(part * 100)
 	if bar.hasRun == true && barCount == bar.barCount && percent == bar.percent {
-		return
+		return "", "", nil
 	}
 	bar.hasRun = true
 	bar.barCount = barCount
 	bar.percent = percent
-	slots := make([]byte, barSize)
+	filledBuf := []byte{}
+	unfilledBuf := []byte{}
 	for i := 0; i < barSize; i++ {
 		if i < barCount {
-			slots[i] = byte('=')
+			filledBuf = append(filledBuf, byte('='))
 		} else {
-			slots[i] = byte(' ')
+			unfilledBuf = append(unfilledBuf, byte('-'))
 		}
 	}
-	fmt.Printf("\r")
-	fmt.Printf(prettyFmt, string(slots), percent)
+	filled += fmt.Sprintf(prettyFmt, string(filledBuf))
 	if percent == 100 {
-		fmt.Println()
+		filled += fmt.Sprintln()
 	}
-	return nil
+	unfilled += string(unfilledBuf)
+	return filled, unfilled, nil
 }
